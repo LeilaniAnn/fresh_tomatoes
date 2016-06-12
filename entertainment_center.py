903115569c5c024e95e8f80408e114ffbce606ed
Leilani Ann Raranga <retrosole@gmail.com>
2016-06-12 13:00:43 -0700

    Added more comments in code and removed unused code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,21 @@
 import media
 import fresh_tomatoes
 
+# Instantiates a movie with params movie_title, movie_storyline, poster_image, trailer_youtube
 toy_story = media.Movie("Toy Story",
 	"A story of a boy and his toys that come to life",
 	"http://img.lum.dolimg.com/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg?region=0,0,300,450",
 	"https://www.youtube.com/watch?v=azyK_k52R1w")
 
-# print toy_story.storyline
-
 avatar = media.Movie("Avatar",
 	"A marine on an alien planet",
 	"https://resizing.flixster.com/PIhRpOR9nW85stswkSet-W-np_w=/800x1200/v1.bTsxMTE3Njc5MjtqOzE3MDU2OzIwNDg7ODAwOzEyMDA",
 	"https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-# print avatar.storyline
 
 warcraft = media.Movie("Warcraft",
 	"Gul'dan is satan",
 	"http://cdn1-www.comingsoon.net/assets/uploads/gallery/warcraft-1387407720/warcraft_ver8_xlg.jpg",
 	"https://www.youtube.com/watch?v=RhFMIRuHAL4")
-# warcraft.show_trailer()
 
 captain_america = media.Movie("Captain America: Civil War",
 	"But he's my friend --so was I",
@@ -30,8 +27,8 @@ the_notebook = media.Movie("The Notebook",
 	"http://sev.h-cdn.co/assets/15/31/1438124471-the-notebook-2004-copy.jpg",
 	"https://www.youtube.com/watch?v=S3G3fILPQAU")
 
+# Appends movie to a list
 movies = [toy_story, avatar, warcraft, captain_america, the_notebook]
+# Opens the movie page with given params in web browser
 fresh_tomatoes.open_movies_page(movies)
 
-print media.Movie.VALID_RATINGS
-print media.Movie.__doc__
--- media.py
+++ media.py
@@ -3,13 +3,14 @@ import webbrowser
 class Movie():
 	""" This class provides a way to store movie related information"""
 	VALID_RATINGS = ["G","PG","PG-13","R"]
-	#Class variables are capitalized
+	# Class variables are capitalized
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-	#Self is object being created, can be named anything but self is convention
-	#Creates space in memory, self allows this to be global
+	# Self is object being created, can be named anything but self is convention
+	# Creates space in memory, self allows this to be global
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 	def show_trailer(self):
+	# Opens trailer video with given url in web browser
 		webbrowser.open(self.trailer_youtube_url)
